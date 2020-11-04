e64aa9a4ab840c3da76a34ae762c195b11505040
Marco Celia <mcelia@minervanetworks.com>
2020-11-04 17:09:18 +0100

    sa

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import pandas as pd
 import numpy as np
 from modularity import modularity
 import math
+import random
+import matplotlib.pyplot as plt
 
+np.random.seed(45678)
 net = pd.read_csv('data/Zachary_Karate_club.txt', sep=' ', header=None)
 
 vertices = sorted(set(list(net[0]) + list(net[1])))
 n = len(vertices)
 
 modularityVEC=[]
-nsim = 200
-for i in range(0,nsim):
-    np.random.seed(123+i)
+nsim = 500
+for i in range(0, nsim):
     sol = list(np.random.choice([1,2],n))
     modularityVEC.append(modularity(net=net, solC=sol))
 
@@ -24,9 +26,35 @@ for i in range(0, nsim-1):
 mm = np.mean(dis)
 T0 = 100*mm
 Tf = mm/100
-nsim = 10000
+nsim = 30000
 A = T0
 B = math.log(Tf/A)/nsim
-Temp = A**(B*np.arange(1,nsim+1))
+Temp = A*np.exp(B*np.arange(1,nsim+1))
 
 modularityPROC = []
+sol = list(np.random.choice([1,2],n))
+solNEW = sol
+modularityBEST = modularity(net=net, solC=solNEW)
+for t in range(0, nsim):
+    nodeRAN = (int)(np.random.choice(vertices, 1)) - 1
+    solNEW[nodeRAN] = 2 if sol[nodeRAN] == 1 else 1
+    modularityNEW = modularity(net=net, solC=solNEW)
+    if (modularityNEW >= modularityBEST):
+        sol = solNEW
+        modularityBEST = modularityNEW
+    else:
+        diff = modularityBEST-modularityNEW
+        p = math.exp(-diff/Temp[t])
+        ran = random.uniform(0, 1)
+        if (ran <= p):
+            sol = solNEW
+            modularityBEST = modularityNEW
+
+    modularityPROC.append(modularityBEST)
+
+print(sol)
+
+# for i in range(0, len(sol)):
+#     print(f"({i+1}, {sol[i]})")
+
+# print(modularityBEST)
--- modularity.py
+++ modularity.py
@@ -2,30 +2,30 @@ import numpy as np
 import pandas as pd
 
 def modularity(*,net,solC):
-    np.random.seed(123)
-
-    vertices = sorted(set(list(net[0]) + list(net[1])))
+    unlist_net = list(net[0]) + list(net[1])
+    vertices = sorted(set(unlist_net))
     n = len(vertices)
     L = len(net)
-    Ki = [(list(net[0]) + list(net[1])).count(i) for i in vertices]
+    Ki = [ unlist_net.count(i) for i in vertices ]
 
     linksCOM = [ (solC[net.loc[i,0]-1], solC[net.loc[i,1]-1]) for i in range(0,len(net)) ]
-    linksCOM = pd.DataFrame(linksCOM, columns=['A','B'])
+    linksCOM = pd.DataFrame(linksCOM, columns=['COM1','COM2'])
 
     e=[]
     for i in range(1,3):
-        mask = (linksCOM['A'] == i) & (linksCOM['B'] == i)
+        mask = (linksCOM['COM1'] == i) & (linksCOM['COM2'] == i)
         subi = linksCOM[mask]
         e.append(len(subi))
 
     er=np.array(e)/L
-    matrCOMK = pd.DataFrame(list(zip(solC, Ki)), columns=['solC','Ki'])
 
+    matrCOMK = pd.DataFrame(list(zip(solC, Ki)), columns=['COM','K'])
     a=[]
     for i in range(1,3):
-        mask = (matrCOMK['solC'] == i)
+        mask = (matrCOMK['COM'] == i)
         subi = matrCOMK[mask]
-        a.append(sum(subi['Ki']))
+        a.append(sum(subi['K']))
+
     ar = np.array(a)/(2*L)
     ar2 = ar**2
     return sum(er-ar2)
